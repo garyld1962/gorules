9c2269f0c17c1bac66a5cc9d0cbff3c16c40df6b
Gary Davidson <GDavidson@vitacost.com>
2016-12-27 17:29:19 -0500

    test cmd exec

---
+++ functionexecutor_test.go
@@ -0,0 +1,38 @@
+package gorules
+
+import (
+  "time"
+      "fmt"
+    "os/exec"
+	"testing"
+	"bufio"
+	"log"
+  "bytes"
+
+)
+
+func TestExternalFile(t *testing.T) {
+   
+  
+  cmd := exec.Command("cmd","/c", "date")
+  stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+  if err := cmd.Start(); err != nil {
+   			log.Fatal(err)
+  	}
+  defer  cmd.Wait()  
+  buf := new(bytes.Buffer)
+	buf.ReadFrom(response.Body)
+  
+  s := string(b)
+  	fmt.Println(s)
+
+  layout := "2006-01-02 15:04:05"
+  ti,_ := time.Parse(layout, s) 
+	fmt.Println(ti.Format("2006-01-02"))
+  //assert.Equal(ti, out, "The current date is: Tue 12/27/2016", "values are not equal")
+
+     
+}
